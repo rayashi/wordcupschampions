e7e9d5a54ac3f38715c77d82bc4926884ac88efa
Humberto Dib Rayashi <rayashi@gmail.com>
2019-04-08 21:03:25 -0300

    Initial Commit

---
+++ WorldCupChampions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldCupChampions
+//
+//  Created by Humberto Rayashi on 08/04/19.
+//  Copyright © 2019 Humberto Rayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
